871ae3d64483bf4183395cdef289433240eb7bf8
Oleh Skrypnykov <oskripnikov@gmail.com>
2021-02-18 20:31:21 +0200

    add :)

--- src/Components/Navtop/Navtop.jsx
+++ src/Components/Navtop/Navtop.jsx
@@ -16,7 +16,7 @@ const Navtop = () => {
                     <Nav.Link href="#Services">Services</Nav.Link>
                     <Nav.Link href="#pricing">Pricing</Nav.Link>
                     <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.1">Action :)</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                         <NavDropdown.Divider />
