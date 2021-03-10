b045d35a137b104824fb7aa8cc191e890ae48743
Oleh Skrypnykov <oskripnikov@gmail.com>
2021-03-10 22:42:54 +0200

    update navbar

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Router.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import {
   HeaderComponent,
   FooterComponent,
